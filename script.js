f359cd84dc0ebb58bc2cf7253a66425846a18d94
espiacent <ch.gripp@posteo.ch>
2022-09-03 23:43:22 +0200

    finish ver1.0

--- script.js
+++ script.js
@@ -10,26 +10,80 @@ const confirm = document.querySelector('.confirm');
 const btn = document.querySelector('button');
 
 form.addEventListener('submit', (e) => {
-    console.log('submit');
     e.preventDefault();
     formValidation();
 });
 
 btn.addEventListener('click', (e) => {
-    console.log('click')
     e.preventDefault();
     formValidation();
 });
 
 function formValidation() {
+    let reMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let reTel = /^\d+$/;
+    let reChars = /^[A-Za-z]+$/;
+    let passOK = false;
     const fnameValue = fname.value.trim();
     const lnameValue = lname.value.trim();
     const mailValue = mail.value.trim();
     const telValue = tel.value.trim();
     const passValue = pass.value.trim();
+    console.log(passValue)
     const confirmValue = confirm.value.trim();
+    console.log(confirmValue)
 
-    if (fnameValue === '') {
-        console.log('fname empty');
+    if (fnameValue.match(reChars) !== null && fnameValue !== '' && fnameValue.length < 32) {
+        showSuccess('fname');
+    }
+    else {
+        showFail('fname');
+    }
+    if (lnameValue.match(reChars) !== null && lnameValue !== '' && lnameValue.length < 32) {
+        showSuccess('lname');
+    }
+    else {
+        showFail('lname');
+    }
+    if (mailValue.match(reMail) !== null && mailValue !== '' && mailValue.length < 48) {
+        showSuccess('mail');
+    }
+    else {
+        showFail('mail');
+    }
+    if (telValue.match(reTel) !== null && telValue.length > 5 && telValue.length < 15) {
+        showSuccess('tel');
+    }
+    else {
+        showFail('tel');
+    }
+    if (passValue !== '' && passValue.length > 7 && passValue.length < 128) {
+        showSuccess('pass');
+        passOK = true;
+    }
+    else {
+        showFail('pass');
+        passOK = false;
+    }
+    if (confirmValue == passValue && confirmValue !== '' && passOK == true) {
+        showSuccess('confirm');
+    }
+    else {
+        showFail('confirm');
     }
 };
+
+function showFail(element) {
+    const errorElement = document.querySelector(`div.${element}.error`);
+    const errorElement2 = document.querySelector(`input.${element}`)
+    errorElement.className = `${element} error true`;
+    errorElement2.className = `${element} notok`;
+};
+
+function showSuccess(element) {
+    const successElement = document.querySelector(`div.${element}.error`);
+    const errorElement2 = document.querySelector(`input.${element}`)
+    successElement.className = `${element} error false`;
+    errorElement2.className = `${element} ok`;
+
+}
